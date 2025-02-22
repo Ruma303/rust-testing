be578233a6e84bae873826e7cd246528f53cbd7f
Ruma <matteoh8@hotmail.it>
2025-02-22 20:50:22 +0100

    .

--- benches/sorting_benchmarks.rs
+++ benches/sorting_benchmarks.rs
@@ -1,22 +1,43 @@
 use testing::sorting_algorithms::{selection_sort, bubble_sort, insertion_sort};
 
-use criterion::{Criterion, criterion_main, criterion_group, black_box};
+use criterion::{Criterion, criterion_main, criterion_group, black_box, BenchmarkId};
 
 // La funzione di benchmarking ottiene l'accesso a un'istanza "c" di Criterion
+// fn sort_benchmark(c: &mut Criterion) {
+//    let mut numbers = vec![1, 2, 3, 4, 5];
+
+//    c.bench_function("Selection Sort", |b|  {
+//        b.iter(|| selection_sort(&mut numbers))
+//    });
+
+//    c.bench_function("Insertion Sort", |b|  {
+//        b.iter(|| insertion_sort(&mut numbers))
+//    });
+
+//    c.bench_function("Bubble Sort", |b|  {
+//        b.iter(|| bubble_sort(&mut numbers))
+//    });
+// }
+
+
 fn sort_benchmark(c: &mut Criterion) {
-   let mut numbers = vec![1, 2, 3, 4, 5];
+    let mut group = c.benchmark_group("Sorting Algorithms");
 
-   c.bench_function("Selection Sort", |b|  {
-       b.iter(|| selection_sort(&mut numbers))
-   });
+    let numbers = vec![5, 3, 8, 4, 2, 11, 1, 32, 23, 7, 10];
 
-   c.bench_function("Insertion Sort", |b|  {
-       b.iter(|| insertion_sort(&mut numbers))
-   });
+    group.bench_with_input(BenchmarkId::new("Selection Sort", numbers.len()), &numbers, |b, data| {
+        b.iter(|| selection_sort(&mut data.clone()))
+    });
 
-   c.bench_function("Bubble Sort", |b|  {
-       b.iter(|| bubble_sort(&mut numbers))
-   });
+    group.bench_with_input(BenchmarkId::new("Insertion Sort", numbers.len()), &numbers, |b, data| {
+        b.iter(|| insertion_sort(&mut data.clone()))
+    });
+
+    group.bench_with_input(BenchmarkId::new("Bubble Sort", numbers.len()), &numbers, |b, data| {
+        b.iter(|| bubble_sort(&mut data.clone()))
+    });
+
+    group.finish();
 }
 
 criterion_group!(sorting_benchmarks, sort_benchmark);
